bd16d17d6ef7d048e481f04742d4b6ee6cdcda81
fatma33 <fatmahesham161@gmail.com>
2019-12-17 00:08:50 +0200

    I moved new functions to collaborators class

--- Sprint4Code/src/Collaborators.java
+++ Sprint4Code/src/Collaborators.java
@@ -3,10 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -64,4 +69,91 @@ public class Collaborators {
    };
  }
 
+  public List < ProductDetails > listSoldProducts() {
+
+        BufferedReader bufferedReader;
+        List < ProductDetails > itemList = new ArrayList < > ();
+      
+        try {
+         bufferedReader = new BufferedReader(new FileReader("SoldProducts.txt"));
+         String line;
+         List < String > lines = new ArrayList < String > ();
+      
+         bufferedReader.lines().forEach(s -> {
+          lines.add(s);
+      
+          if (lines.size() == 3) {
+           itemList.add(new ProductDetails(lines.get(1), lines.get(0), lines.get(2)));
+           lines.clear();
+          }
+      
+         });
+      
+         bufferedReader.close();
+        } catch (FileNotFoundException e) {
+        //  e.printStackTrace();
+      
+        } catch (IOException e) {
+        //  e.printStackTrace();
+        }
+      
+        return itemList;
+       }
+  
+  
+  
+ public void commitChange(List < ProductDetails > itemList  ) throws IOException
+ {
+
+        if (itemList!=null){
+                File usersFile = new File("Aproducts.txt");
+                FileWriter writer = new FileWriter(usersFile);
+                for (ProductDetails record: itemList) {
+                        writer.write(record.getProductName()+"\n" +
+                        
+                        record.getCategory() + "\n" 
+                        
+                        + record.getPrice() + "\n");
+                       }
+                    
+                       writer.close();
+          }
+ }
+
+ /**
+  * 
+  */
+ public List < ProductDetails > listAllProducts() {
+
+  BufferedReader bufferedReader;
+  List < ProductDetails > itemList = new ArrayList < > ();
+
+  try {
+   bufferedReader = new BufferedReader(new FileReader("Aproducts.txt"));
+   String line;
+   List < String > lines = new ArrayList < String > ();
+
+   bufferedReader.lines().forEach(s -> {
+    lines.add(s);
+
+    if (lines.size() == 3) {
+     itemList.add(new ProductDetails(lines.get(0), lines.get(1), lines.get(2)));
+     lines.clear();
+    }
+
+   });
+
+   bufferedReader.close();
+  } catch (FileNotFoundException e) {
+   e.printStackTrace();
+
+  } catch (IOException e) {
+   e.printStackTrace();
+  }
+
+  return itemList;
+ }
+
+
+ 
 }
